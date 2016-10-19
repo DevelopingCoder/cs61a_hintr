2bc7b4ba22a33c614b46ed75adddb730d4f7f472
Kevin <theguo007@berkeley.edu>
2016-10-19 05:49:54 +0000

    added user steps for 'given i log in with email and pass...'

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -9,11 +9,14 @@ Given /^I am on (.*)$/ do |page|
     step("I visit #{page}")
 end
 
-Given /^I log in with email: "(.*)" and password: "(.*)"$/ do
-    pending
-end
-
-Then /^I should see (\d+) users?$/ do |num|
-    expect(all('tr#user').count).to eq num
-end
+Given /^I log in with email: "(.*)" and password: "(.*)"$/ do |email, pass|
+    visit path_to("login")
+    fill_in("user_email", :with => email)
+    fill_in("user_password", :with => pass)
+    click_button("commit")
+end    
+    
+# Then /^I should see (\d+) users?$/ do |num|
+#     expect(all('tr#user').count).to eq num
+# end
 
