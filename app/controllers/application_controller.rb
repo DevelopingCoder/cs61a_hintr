fecb71b9458c3585ce15cc3b64357e1f932d515d
Jayanth Devarajan <jaysid95@berkeley.edu>
2016-10-25 23:14:06 +0000

    random password generation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    # devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:account_update) { |u| 
+      u.permit(:password, :password_confirmation, :current_password, :name) 
+    }
   end
 end
