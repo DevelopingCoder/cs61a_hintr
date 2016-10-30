fe927b02acbacd5cfea7bb29e30a0304344b4fb3
Anthony Tan <atan5133@berkeley.edu>
2016-10-30 23:59:31 +0000

    --amend

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,5 @@ class Tag < ActiveRecord::Base
     validates :name, presence: true
     valides :description, presence: true
     has_many :tag2concepts
+    has_many :concepts, :through => :tag2concepts
 end
