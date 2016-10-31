4397d8f2dc81bdb679971b82e41235d923a00b14
Anthony Tan <atan5133@berkeley.edu>
2016-10-31 01:37:04 +0000

    created route for upload and controller method. added bootstrap to certain elements

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,3 @@ class PagesController < ApplicationController
         end
     end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -8,57 +8,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root to: 'pages#index'
-  
-  match '/display_users', to: 'users#index', via: :get
-  match '/display_users', to: 'users#create', via: :post
-  match '/display_users', to: 'users#destroy', via: :put
-  match '/display_users/:id', to: 'users#edit', via: :post
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  match '/display_users', to: 'pages#display_users', via: :get
+  match '/display_users', to: 'pages#add_user', via: :post
+  match '/display_users', to: 'pages#delete_user', via: :put
+  match '/upload', to: 'pages#upload_menu', via: :get
 end
