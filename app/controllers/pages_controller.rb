97a25e79795d50b5b06a293481cf612a234c818a
Anthony Tan <atan5133@berkeley.edu>
2016-10-31 00:56:29 +0000

    fixed the signed out successfully not showing up bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,24 @@
 require 'securerandom'
 
 class PagesController < ApplicationController
-    before_action :authenticate_user!
+    # Authenticate_user does what require login does but yields the message "user must be signed up for that action"
+    # the reason why index has require_login is because when you logout, you automatically are routed to index of this controller
+    # and it still needs to route to user signed in while saying "signed out successfully"
+    before_action :authenticate_user!, except: [:index]
+    before_action :require_login, only: [:index]
+ 
+    def require_login
+        if not (user_signed_in?)
+            redirect_to new_user_session_path # halts request cycle
+        end
+    end
     
     def add_user
         if current_user.admin?
             email = params[:add_email]
             # password = generate_password
             password = SecureRandom.urlsafe_base64(6)
-            name = 'Change Me'
+            name = ''
             #Check if user is already added
             success = User.create({:name=>name, :email => email, :password => password})
             if success.id
@@ -38,6 +48,10 @@ class PagesController < ApplicationController
             end
         end
         redirect_to display_users_path
+    end
+    
+    def generate_password
+        'password'    
     end
     
     def display_users
