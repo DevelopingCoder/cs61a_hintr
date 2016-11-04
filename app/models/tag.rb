ec117a2d71114ab4287aba1afac98d2b47c5e452
Kevin <theguo007@berkeley.edu>
2016-11-04 06:24:54 +0000

    changed valides to validates. typo

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
     validates :name, presence: true
-    valides :description, presence: true
+    validates :description, presence: true
     has_many :tag2concepts
     has_many :concepts, :through => :tag2concepts
 end
