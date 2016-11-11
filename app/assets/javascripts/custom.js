c401cb135610d416890234b2696b2cba83b28d36
yashleya <ashley.chen@berkeley.edu>
2016-11-10 23:12:20 -0800

    Finish concepts (#12)
    * fix threshold bug and show tags on concepts
    
    * upvote downvote triggers ajax
    
    * Question set page features and minor changes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,4 @@
-function toggle_admin(id){
+function toggle_admin(id) {
     var data = {
         field: "admin",
         value: event.target.checked
@@ -15,11 +15,37 @@ function toggle_admin(id){
     });
 }
 
-function verify_delete(){
+function verify_delete() {
     alertify.confirm("Are you sure you want to delete? There is no undo because that's a lot of work.",
     function(){
         $("#delete_user_form").submit()
     },
     function(){
     });
+}
+
+function upvote(message_id, concept_id) {
+    var data = {
+        field: "message_id",
+        value: message_id
+    }
+    $.ajax({
+        url: '/concepts/' + concept_id.to_s + '/messages/' + message_id.to_s + '/upvote',
+        type: 'POST',
+        dataType: "application/json",
+        data: JSON.stringify(data)
+    })
+}
+
+function downvote(message_id, concept_id) {
+    var data = {
+        field: "message_id",
+        value: message_id
+    }
+    $.ajax({
+        url: '/concepts/' + concept_id.to_s + '/messages' + message_id.to_s + '/downvote',
+        type: 'POST',
+        dataType: "application/json",
+        data: JSON.stringify(data)
+    })
 }
