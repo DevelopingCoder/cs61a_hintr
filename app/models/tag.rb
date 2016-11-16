03cdbe0c224564e81519a45bf8bb54a440707867
Ashley Chen <ashley.chen@berkeley.edu>
2016-11-16 02:16:27 +0000

    set up dbs for questions and wrong answers

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,7 @@ class Tag < ActiveRecord::Base
     validates :name, presence: true
     validates :description, presence: true
     has_many :tag2concepts
+    has_many :tag2wronganswers
     has_many :concepts, :through => :tag2concepts
+    has_many :wrong_answers, :through => :tag2wronganswers
 end
