05fad82fd9edf7bfeec02060eda9e81c84a60246
Jayanth Devarajan <jaysid95@berkeley.edu>
2016-11-16 02:23:39 +0000

    finishing up questions tests

--- features/step_definitions/question_steps.rb
+++ features/step_definitions/question_steps.rb
@@ -1,15 +1,25 @@
-Given /^the following Questions? exists?:$/ do |question_table|
-    pending
+Given /^the following Questions? exists?:$/ do |questions_table|
+    questions_table.hashes.each do |question| 
+        Question.create!(question)
+    end
 end
 
-Given /^the following Wrong Answers? exists?:$/ do |question_table|
-    pending
+Given /^the following Wrong Answers? exists?:$/ do |wrong_answers_table|
+    wrong_answers_table.hashes.each do |wrong_answer|
+        Wrong_Answer.create!(wrong_answer)
+    end
 end
 
-Given /^the following Tags? exists?:$/ do |question_table|
-    pending
+Given /^the following Tags? exists?:$/ do |tags_table|
+    tags_table.hashes.each do |tag|
+        Tag.create!(tag)
+    end
 end
 
-Given /^the following hints? exists?:$/ do |question_table|
-    pending
+Given /^the following Hints? exists?:$/ do |hints_table|
+    hints_table.hashes.each do |hint|
+        Hint.create!(hint)
+    end
 end
+
+
