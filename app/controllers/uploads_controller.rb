af120aef71b5a8a69b2fa4a59de053bb553b2fb9
Anthony Tan <atan5133@berkeley.edu>
2016-11-18 04:02:23 +0000

    added minor change to upload_controller to expand

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -4,6 +4,6 @@ class UploadsController < ApplicationController
     def new
         #Based on file being uploaded, redirect to its upload controller
         file_type = params[:file_type].split()[0].downcase #(ie Concept, Tag)
-        redirect_to(controller: :upload_concepts, action: :show, path: params[:file].path)
+        redirect_to(controller: "upload_" + file_type, action: :show, path: params[:file].path)
     end
 end
