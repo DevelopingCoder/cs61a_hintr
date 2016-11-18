9aa7ec340ce6e9de9c75119bbd2e098511c8aad8
Jayanth Devarajan <jaysid95@berkeley.edu>
2016-11-18 05:42:59 +0000

    upload_questions finishing up view code

--- app/models/tag2wronganswer.rb
+++ app/models/tag2wronganswer.rb
@@ -1,7 +1,7 @@
 class Tag2wronganswer < ActiveRecord::Base
     validates :tag, presence: true
     validates :wrong_answer, presence: true
-    belongs_to :tag
-    belongs_to :wrong_answer
+    belongs_to :tag, :dependent => :destroy
+    belongs_to :wrong_answer, :dependent => :destroy
     has_many :hints
 end
