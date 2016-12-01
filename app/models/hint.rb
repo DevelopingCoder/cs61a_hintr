dc819dcda4c41519cd1542c723fd5dde36b82996
Anthony Tan <atan5133@berkeley.edu>
2016-12-01 04:04:20 +0000

    Added validations for hints

--- app/models/hint.rb
+++ app/models/hint.rb
@@ -1,6 +1,15 @@
 class Hint < ActiveRecord::Base
     belongs_to :tag2wronganswer
     validates :content, presence: :true
-    has_many :hint_votes
+    validate :is_unique
+    has_many :hint_votes, dependent: :destroy
     has_many :users, :through => :hint_votes
+    
+    def is_unique
+        tag2wronganswer.hints.each do |hint|
+            if hint.content == self.content
+                errors.add(:hint, "already exists for the wa_tag")
+            end
+        end
+    end
 end
