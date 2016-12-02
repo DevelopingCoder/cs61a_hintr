c0d56b10cc2ae670b9a85181e7af720192673db3
Anthony Tan <atan5133@berkeley.edu>
2016-12-02 06:39:23 +0000

    DRYed out upload controllers

--- app/controllers/upload_tags_controller.rb
+++ app/controllers/upload_tags_controller.rb
@@ -1,21 +1,9 @@
 class UploadTagsController < ApplicationController
     
     def show
-        if not params.keys.include?("path")
-            flash[:notice] = "Oops we lost your state. Please upload again"
-            redirect_to upload_path and return
-        end
-        changes = Tag.import(params[:path])
-        if not changes
-            flash[:notice] = "Tag file not correctly formatted. First 10 columns must be 
+        format_msg = "Tag file not correctly formatted. First 10 columns must be 
                 Old tag name,cp,Status,Tag Name,Description,Example,Primary Concept,Topic,Count in Tag to Concept Master,Concepts"
-            redirect_to upload_path and return
-        elsif changes.key? :error
-            @error = changes[:error]
-        end
-        @additions = changes[:additions]
-        @deletions = changes[:deletions]
-        @edits = changes[:edits]
+        show_changes(Tag, format_msg)
     end
     
     def confirm
