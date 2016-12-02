b072a41068649571b00b49d537c7d43baa673960
Anthony Tan <atan5133@berkeley.edu>
2016-12-02 05:51:43 +0000

    Fixed sorting code for GPA purposes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, unless: :is_devise?
+  helper_method :sort_messages
   
   def is_devise?
     return params[:controller].include?("devise/")
@@ -17,6 +18,38 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) { |u| 
       u.permit(:password, :password_confirmation, :current_password, :name) 
     }
+  end
+  
+  def sort_messages(sort_type, messages, is_hint)
+    message_upvotes = {}
+    message_downvotes = {}
+    messages.each do |message|
+        upvotes = 0
+        downvotes = 0
+        if is_hint
+          votes = message.hint_votes 
+        else
+          votes = message.votes
+        end
+        votes.each do |vote|
+            if vote.vote_type > 0
+                upvotes += 1
+            elsif vote.vote_type < 0
+                downvotes += 1
+            end
+        end
+        #Negate because we sort by smallest to largest
+        message_upvotes[message] = -upvotes
+        message_downvotes[message] = -downvotes
+    end
+    
+    if sort_type == "upvotes"
+        messages = message_upvotes.sort_by{|_k,v| v}.map{|_k,v| k}
+    elsif sort_type == "downvotes"
+        messages = message_downvotes.sort_by {|_k,v| v}.map{|_k,v| k}
+    end
+    
+  return messages
   end
   
 end
